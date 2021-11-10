e2689a0a5df9db367909abd9a00f05ede9f71a5c
Igor-Cherepanov <cherepanov@batyukovstudio.com>
2021-11-10 23:32:21 +0500

    feat(products): c

--- app/Http/Controllers/CRM/ProductController.php
+++ app/Http/Controllers/CRM/ProductController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\CRM;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+
+    public function show(Request $request, Product $product){
+
+        return view('products.show', compact('product'));
+    }
+
 }
