6c3e1dcb1782efb8a9820a402b264e21a69f773a
Igor-Cherepanov <cherepanov@batyukovstudio.com>
2021-11-12 23:00:37 +0500

    minor

---
+++ app/Console/Commands/Debug.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Console\Command;
+
+class Debug extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'debug';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Category::where('id', '>', 0)->delete();
+        Product::where('id', '>', 0)->delete();
+        return Command::SUCCESS;
+    }
+}
