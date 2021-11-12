7c52a68ade6889530dcf4a065eef55d6c6f9a0b2
Igor-Cherepanov <cherepanov@batyukovstudio.com>
2021-11-12 23:51:26 +0500

    minor

--- app/Http/Controllers/CRM/ProductController.php
+++ app/Http/Controllers/CRM/ProductController.php
@@ -10,21 +10,6 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
 
-    public function index(Request $request, ?Category $category)
-    {
-        if (isset($category)) {
-            $products = $category->getProducts();
-        } else {
-            $products = Product::get();
-        }
 
-        return view('site.products.index', compact('products'));
-    }
-
-    public function show(Request $request, Product $product)
-    {
-
-        return view('products.show', compact('product'));
-    }
 
 }
