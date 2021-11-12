5c24a0f276c6e4dc10bb3c03a2a373f1534b393e
Igor-Cherepanov <cherepanov@batyukovstudio.com>
2021-11-12 23:56:02 +0500

    minor

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
     public function index(Request $request, Category $category)
     {
         $products = $category->getProducts();
-
+        $products = Product::all();
         return view('site.products.index', compact('products'));
     }
 
